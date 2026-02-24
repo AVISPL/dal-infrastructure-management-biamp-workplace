5a7992cacd5fda4d2274527474d9b549428fbe38
maksym.rossiitsev <maksym.rossiitsev@avispl.com>
2026-02-24 14:27:56 +0200

    SYAL-2643 Monitoring rate support + metadata format fixes

--- src/main/java/com/avispl/symphony/dal/infrastructure/management/biamp/workplace/types/aggregator/GeneralProperty.java
+++ src/main/java/com/avispl/symphony/dal/infrastructure/management/biamp/workplace/types/aggregator/GeneralProperty.java
@@ -17,7 +17,6 @@ public enum GeneralProperty implements BaseProperty {
 	ADAPTER_UPTIME_MIN("AdapterUptime(min)", "adapter.uptime"),
 	ADAPTER_VERSION("AdapterVersion", "adapter.version"),
 	LAST_MONITORING_CYCLE_DURATION("LastMonitoringCycleDuration(sec)", "adapter.cycle.duration"),
-	MONITORING_CYCLE_INTERVAL("MonitoringCycleInterval(min)", "monitoring.cycle.interval"),
 	MONITORED_DEVICES_TOTAL("MonitoredDevicesTotal", "adapter.devices.total");
 
 	private final String name;
